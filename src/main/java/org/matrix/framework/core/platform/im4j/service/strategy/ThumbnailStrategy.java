ae103302914b07ebc2bcd039f150c05fc77cb359
qaehealth2021 <115323741+qaehealth2021@users.noreply.github.com>
2022-11-21 14:46:17 -0800

    Delete src/main/java/org/matrix/framework/core directory

--- src/main/java/org/matrix/framework/core/platform/im4j/service/strategy/ThumbnailStrategy.java
+++
@@ -1,6 +0,0 @@
-package org.matrix.framework.core.platform.im4j.service.strategy;
-
-
-public interface ThumbnailStrategy  {
-    void thumbnailImage(String originalImgPath, double imgPdi, long width, long height, String destImgPath)  throws Exception;
-}
